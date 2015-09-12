031726938113e05e628ea402f05dafb28c56818a
JiaTeng <anthony1573@126.com>
2015-09-12 23:06:45 +0800

    12122

--- GitProject/src/Test.java
+++ GitProject/src/Test.java
@@ -1,7 +1,7 @@
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hillo Word!");
+		System.out.println("Hillo Word!");12211221
 		System.out.println("Hillo Word123!");
 	}
 }
